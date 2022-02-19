145f73263e67cbf38ccf49b51a45ed2fbfe8cb8d
Peihai Luo （罗培海） <phluo@trip.com>
2022-02-19 22:19:09 +0800

    feat: footer 完成

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,20 +1,47 @@
+import { useState, useEffect } from 'react'
 import Link from '../link'
 import siteMetadata from '@/data/siteMetadata'
 import SocialIcon, { EKind } from '@/components/social-icons'
 import { Popover } from 'antd'
+import { get } from '@/lib/api'
+import { IRespones, IUser } from '@/interface/user'
 
 export default function Footer() {
+  const [user, setUser] = useState<IUser | {}>({})
   const content = <div>啦啦啦</div>
+  useEffect(function () {
+    ;(async function () {
+      const { data }: IRespones = await get('/user')
+      if (data.isSuccess) {
+        setUser(data.data)
+      }
+    })()
+  }, [])
+  console.log('data', user)
   return (
     <footer>
       <div className="flex flex-col items-center mt-16">
         <div className="flex mb-3 space-x-4">
-          <Popover placement="top" content={content} trigger="click">
+          <Popover
+            placement="top"
+            content={<img src={user?.wx} />}
+            trigger="click"
+          >
             <SocialIcon kind={EKind.wx} />
           </Popover>
-          <SocialIcon kind={EKind.tel} href={`mailto:${siteMetadata.email}`} />
-          <SocialIcon kind={EKind.qq} href={`mailto:${siteMetadata.email}`} />
-          <SocialIcon kind={EKind.mail} href={`mailto:${siteMetadata.email}`} />
+          <Popover placement="top" content={user?.tel} trigger="click">
+            <SocialIcon kind={EKind.tel} />
+          </Popover>
+          <Popover
+            placement="top"
+            content={<img src={user?.qq} />}
+            trigger="click"
+          >
+            <SocialIcon kind={EKind.qq} />
+          </Popover>
+          <Popover placement="top" content={user?.email} trigger="click">
+            <SocialIcon kind={EKind.mail} />
+          </Popover>
         </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>lx</div>
---
+++ interface/user/index.ts
@@ -0,0 +1,27 @@
+import { IPaginate } from '../common'
+import { AxiosResponse } from 'axios'
+/**
+ * 返回结构通用信息
+ */
+export interface IRespones extends AxiosResponse {
+  data: IDataRes
+}
+
+export interface IDataRes {
+  isSuccess: boolean
+  message?: string
+  data: IUser
+}
+
+export interface IUser {
+  username: string
+  nickname: string
+  avatar: string
+  email: string
+  sign: string
+  introduce: string
+  tel: string
+  qq: string
+  wx: string
+  create_time: string
+}
