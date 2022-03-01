5652592d9706f24e8df7c07b1eac6025e05b7577
Peihai Luo （罗培海） <phluo@trip.com>
2022-03-01 08:27:06 +0800

    feat: 关于我page页面完成

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,6 +1,12 @@
+import { NextPage } from 'next'
 import Image from '@/components/image'
+import { get } from '@/lib/api'
+import { IRespones, IUser } from '@/interface/user'
+interface IProps {
+  user?: IUser
+}
 
-export default function About({}) {
+const About: NextPage<IProps> = ({ user }) => {
   return (
     <main className="mb-auto">
       <div className="divide-y">
@@ -15,35 +21,37 @@ export default function About({}) {
               width={150}
               height={150}
               alt="avatar"
-              src="/avatar.png"
+              src={user?.avatar || ''}
               className={`rounded-full`}
             />
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">
-              LX
+              {user?.nickname}
             </h3>
-            <div className="text-gray-500 dark:text-gray-400">UI设计师</div>
-            <div className="text-gray-500 dark:text-gray-400">菜来湿乐公司</div>
+            {user?.sign && (
+              <div className="text-gray-500 dark:text-gray-400">
+                {user.sign}
+              </div>
+            )}
+
             <div className="flex pt-6 space-x-3"></div>
           </div>
           <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
-            <p>
-              滚滚长江东逝水 2，浪花淘尽英雄 3。是非成败转头空 4。青山依旧在
-              5，几度夕阳红 6。 白发渔樵江渚上 7，惯看秋月春风 8。一壶浊酒喜相逢
-              9。古今多少事 10，都付笑谈中 11。
-            </p>
-            <p>
-              滚滚长江东逝水 2，浪花淘尽英雄 3。是非成败转头空 4。青山依旧在
-              5，几度夕阳红 6。 白发渔樵江渚上 7，惯看秋月春风 8。一壶浊酒喜相逢
-              9。古今多少事 10，都付笑谈中 11。
-            </p>
-            <p>
-              滚滚长江东逝水 2，浪花淘尽英雄 3。是非成败转头空 4。青山依旧在
-              5，几度夕阳红 6。 白发渔樵江渚上 7，惯看秋月春风 8。一壶浊酒喜相逢
-              9。古今多少事 10，都付笑谈中 11。
-            </p>
+            {user?.introduce && <p>{user.introduce}</p>}
           </div>
         </div>
       </div>
     </main>
   )
 }
+
+About.getInitialProps = async (ctx) => {
+  try {
+    const { data }: IRespones = await get('/user')
+    return data.isSuccess ? { user: data.data } : {}
+  } catch (error) {
+    console.log(error)
+  }
+  return {}
+}
+
+export default About
