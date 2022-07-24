e9175b04cc30a8f4592915e78b849762eeb8ed70
Peihai Luo （罗培海） <phluo@trip.com>
2022-07-24 11:54:19 +0800

    feat: loader修改

--- next.config.js
+++ next.config.js
@@ -29,7 +29,15 @@ module.exports = withBundleAnalyzer(
       webpack: (config, { dev, isServer }) => {
         config.module.rules.push({
           test: /\.(woff|woff2|eot|ttf|svg)$/,
-          loader: 'url-loader?limit=100000',
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 1000,
+                name: 'img/[name].[ext]',
+              },
+            },
+          ],
         })
 
         return config
